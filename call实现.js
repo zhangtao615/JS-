f18fef4497f1c97cbc8aaef6ea9982057d8a7dac
zhangtao615 <295564283@qq.com>
2020-06-28 20:47:17 +0800

    apply

--- call实现.js
+++ call实现.js
@@ -1,8 +1,13 @@
-Function.prototype.call = function(context){
+/*
+将函数设为对象的属性
+执行该函数
+从对象上删除该函数
+*/
+Function.prototype.call = (context) =>{
     if(typeof this !== 'function'){
         throw new TypeError('not a function');
     }
-    //处理context可能为空的情况 
+    //处理context可能为空的情况
     context = context || window;
     context.fn = this;
     let arg = [...this.arguments].slice(1);
--- 实现new.js
+++ 实现new.js
@@ -1,11 +1,11 @@
-function New(fun){
-    return function(){
+const New = (fn) => {
+    return () => {
         //创建一个新对象且将其隐式原型指向构造函数原型
         let obj = {
-            __proto__ :fun.prototype
+            __proto__ = fn.prototype
         }
         //执行构造函数
-        fun.call(obj,...arguments);
+        fn.call(obj,arguments);
         //返回对象
         return obj;
     }
